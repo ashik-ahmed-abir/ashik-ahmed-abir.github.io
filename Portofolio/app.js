9684e3d4a7fee6119c2323ef526b411acfad0405
abir-ashik-abir <abirashik11@gmail.com>
2021-07-05 12:58:05 +0600

    .

--- Portofolio/app.js
+++ Portofolio/app.js
@@ -24,7 +24,82 @@ menuCloseBtn.addEventListener('click', () => {
     closeMenu();
 });
 
-// GSAP
+// CONTACT
 
-gsap.from('body', {duration: 3, opacity: 0, delay: 1});
-gsap.from('#link-collection a', {x: -200, oppacity: 0, duration: 1, delay: 3, stagger: 0.1 });
+const logoIcon = document.querySelector('#con-logo');
+const logoText = document.querySelector('#con-text');
+
+logoIcon.addEventListener("mouseover", () => {
+    logoText.classList.remove('hidden');
+    logoIcon.classList.remove('bg-black');
+    logoIcon.classList.add('bg-blue-700');
+    
+ 
+});
+
+logoIcon.addEventListener("mouseout", () => {
+    logoText.classList.add('hidden');
+    logoIcon.classList.add('bg-black');
+    logoIcon.classList.remove('bg-blue-700');
+    
+});
+
+
+
+
+
+// HTML CONTACT FORM
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks!";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+  
+
+  // GSAP
+
+gsap.from('body', {duration: 2, opacity: 0, delay: 1});
+gsap.from('#link-collection a', {x: -200, oppacity: 0, duration: 1, delay: 1, stagger: 0.1 });
+
+
